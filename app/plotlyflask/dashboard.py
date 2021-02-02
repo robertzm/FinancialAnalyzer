8ece2d9ffe94ac74eb885f637c7c660d5abfdacf
Kimi Meng <kimimeng2013@gmail.com>
2021-02-02 01:50:08 -0800

    added plotly for monthly cost display

--- app/plotlyflask/dashboard.py
+++ app/plotlyflask/dashboard.py
@@ -3,12 +3,10 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 import dash_table
-import numpy as np
-import pandas as pd
+from dash.dependencies import Input, Output
 
 from .data import create_dataframe
 from .layout import html_layout
-
 
 def init_dashboard(server):
     """Create a Plotly Dash dashboard."""
@@ -24,35 +22,31 @@ def init_dashboard(server):
     # Load DataFrame
     df = create_dataframe()
 
+    date = list(sorted(set(df.timeslot)))
+    categories = list(set(df.category))
+    amountAvg = [df[df['category'] == i]['amount'].sum() / len(date) for i in categories]
+
     # Custom HTML layout
     dash_app.index_string = html_layout
 
     # Create Layout
     dash_app.layout = html.Div(
         children=[
-            dcc.Graph(
-                id="histogram-graph",
-                figure={
-                    "data": [
-                        {
-                            "x": df["complaint_type"],
-                            "text": df["complaint_type"],
-                            "customdata": df["key"],
-                            "name": "311 Calls by region.",
-                            "type": "histogram",
-                        }
-                    ],
-                    "layout": {
-                        "title": "NYC 311 Calls category.",
-                        "height": 500,
-                        "padding": 150,
-                    },
-                },
+            dcc.Graph(id="histogram-graph"),
+            dcc.Slider(
+                id='month-slider',
+                marks={i: '{}/{}'.format(date[i].year, date[i].month) for i in range(len(date))},
+                min=0,
+                max=len(date)-1,
+                step=1,
+                value=2
             ),
             create_data_table(df),
         ],
         id="dash-container",
     )
+    init_callbacks(dash_app, categories, date, amountAvg, df)
+
     return dash_app.server
 
 
@@ -67,3 +61,23 @@ def create_data_table(df):
         page_size=300,
     )
     return table
+
+
+def init_callbacks(dash_app, categories, date, amountAvg, df):
+    @dash_app.callback(Output('histogram-graph', 'figure'), [Input('month-slider', 'value')])
+    def make_figure(value):
+        tmp = df[df['timeslot'] == date[value]]
+        amount = [tmp[tmp['category'] == i]['amount'].sum() for i in categories]
+
+        figure = {
+            "data": [
+                {'x': categories, 'y': amount, 'type': 'bar', 'name': 'MonthlyCost'},
+                {'x': categories, 'y': amountAvg, 'type': 'bar', 'name': 'AvgCost'}
+            ],
+            "layout": {
+                "title": "Monthly Cost",
+                "height": 500,
+                "padding": 150,
+            },
+        }
+        return figure
--- app/plotlyflask/data.py
+++ app/plotlyflask/data.py
@@ -1,14 +1,11 @@
 """Prepare data for Plotly Dash."""
-import numpy as np
 import pandas as pd
+from app import db
 
 
 def create_dataframe():
     """Create Pandas DataFrame from local CSV."""
-    df = pd.read_csv("data/311-calls.csv", parse_dates=["created"])
-    df["created"] = df["created"].dt.date
-    df.drop(columns=["incident_zip"], inplace=True)
-    num_complaints = df["complaint_type"].value_counts()
-    to_remove = num_complaints[num_complaints <= 30].index
-    df.replace(to_remove, np.nan, inplace=True)
-    return df
+    df = pd.read_sql("SELECT * from TransRecord", db.get_engine(), parse_dates=["timeslot", "date"])
+    df["timeslot"] = df["timeslot"].dt.date
+    df.drop(columns=["uuid", "date", "description", "uploadfile"], inplace=True)
+    return df[df['gain'] == 0]
