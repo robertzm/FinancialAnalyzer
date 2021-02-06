e314fb4dd30a9b4f4f1424f094f7a5a74d7e6823
Kimi Meng <kimimeng2013@gmail.com>
2021-02-06 01:16:08 -0800

    testing to add income vs. cost vs. investment graph

--- app/plotlyflask/dashboard.py
+++ app/plotlyflask/dashboard.py
@@ -4,13 +4,16 @@ import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
 import plotly.graph_objs as go
+from plotly.subplots import make_subplots
 
-from .data import create_dataframe
+from .data import create_cost_dataframe, create_all_dataframe
 
 dash_url = "/dash/app1/"
-df = create_dataframe()
+df = create_cost_dataframe()
 categories = list(set(df.category))
 date = list(sorted(set(df.timeslot)))
+
+
 # amountAvg = [df[df['category'] == i]['amount'].sum() / len(date) for i in categories]
 
 def init_dashboard(server):
@@ -33,7 +36,8 @@ def init_dashboard(server):
                        step=1,
                        value=2),
             html.Button('Refresh', id='button'),
-            dcc.Graph(id="monthly-cost-pct-graph")
+            dcc.Graph(id="monthly-cost-pct-graph"),
+            dcc.Graph(id="monthly-ICI-graph")  # income / cost / invest
         ],
         id="dash-container",
     )
@@ -76,11 +80,45 @@ def init_callbacks(dash_app):
                         })
         return fig
 
+    @dash_app.callback(Output('monthly-ICI-graph', 'figure'), [Input('button', 'n_clicks')])
+    def make_ICI_bar_figure(value):
+        fig = make_subplots(specs=[[{"secondary_y": True}]])
+
+        df = create_all_dataframe()
+        date = list(sorted(set(df.timeslot)))
+        cost = ["Restaurant", "Grocery", "Utilities", "Clothes",
+                "HouseImprovement", "Vacation", "Gas", "Cars",
+                "Play", "Medicals", "White", "Parents", "Shits"]
+
+        cost_amount = [df[(df['timeslot'] == i)][(df['category'].isin(cost))]['amount'].sum() for i in date]
+        income_amount = [df[df['timeslot'] == i][df['category'] == 'Income']['amount'].sum() for i in date]
+        invest_amount = [df[df['timeslot'] == i][df['category'] == 'Invest']['amount'].sum() for i in date]
+
+        trace1 = go.Bar(
+            x = date,
+            y = income_amount,
+            name = 'Income'
+        )
+        trace2 = go.Bar(
+            x = date,
+            y = cost_amount,
+            name = 'Cost'
+        )
+        trace3 = go.Scatter(
+            x = date,
+            y = invest_amount,
+            name = 'Invest'
+        )
+
+        fig.add_trace(trace1, secondary_y=False)
+        fig.add_trace(trace2, secondary_y=False)
+        fig.add_trace(trace3, secondary_y=True)
+
+        return fig
 
     @dash_app.callback(Output('month-slider', 'marks'), Output('month-slider', 'max'), Input('button', 'n_clicks'))
     def update_slider(value):
-        global df, categories, date, amountAvg
-        df = create_dataframe()
+        df = create_cost_dataframe()
         categories = list(set(df.category))
         date = list(sorted(set(df.timeslot)))
         # amountAvg = [df[df['category'] == i]['amount'].sum() / len(date) for i in categories]
--- app/plotlyflask/data.py
+++ app/plotlyflask/data.py
@@ -3,9 +3,16 @@ import pandas as pd
 from app import db
 
 
-def create_dataframe():
+def create_cost_dataframe():
     """Create Pandas DataFrame from local CSV."""
     df = pd.read_sql("SELECT * from TransRecord", db.get_engine(), parse_dates=["timeslot", "date"])
     df["timeslot"] = df["timeslot"].dt.date
     df.drop(columns=["uuid", "date", "description", "uploadfile"], inplace=True)
     return df[df['gain'] == 0]
+
+def create_all_dataframe():
+    """Create Pandas DataFrame from local CSV."""
+    df = pd.read_sql("SELECT * from TransRecord", db.get_engine(), parse_dates=["timeslot", "date"])
+    df["timeslot"] = df["timeslot"].dt.date
+    df.drop(columns=["uuid", "date", "description", "uploadfile"], inplace=True)
+    return df
